5e3121d0d424f4b584c57ee5f3b1b4e963a2121f
Pazuzu Jindrich <Pazuzujindrich@gmail.com>
2019-06-03 20:42:00 -0700

    Update SensorInter.java

--- src/SensorInter.java
+++ src/SensorInter.java
@@ -2,6 +2,8 @@
 * The interface of the adapters.
 * */
 public interface SensorInter {
+    //read(), report(), and type() functions are shared by all three sensors
+    //interface should catpure this
     void uiPart(UiFunction uf);
     }
 
